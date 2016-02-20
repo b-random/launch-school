9479a623aa870285b05dd406bcc4ab8d2259e682
Brandon Scott Brigance <brandon.brigance@gmail.com>
2016-02-20 01:05:31 +0000

    after code review

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,96 +1,93 @@
 VALID_CHOICES = %w(rock paper scissors lizard spock)
                      # this is another way to write an array
-$player_score = 0
-$computer_score = 0
+ABBREVIATED_CHOICES = %w(ro pa sc li sp)
 
 def prompt(message)
-  Kernel.puts(message.to_s)
+  puts("#{message}")
 end
 
+def change_initial(convert)
+  case convert
+  when 'ro' then 'rock'
+  when 'pa' then 'paper'  
+  when 'sc' then 'scissors'  
+  when 'li' then 'lizard'
+  when 'sp' then 'spock'  
+  end
+end
 
-def win?(first, second) 
-  (first == 'rock' && second == 'scissors') || (first == 'rock' && second == 'lizard') ||
+def winner?(first, second)
+  (firtst == 'rock' && second == 'scissors') || (first == 'rock' && second == 'lizard') ||
   (first == 'paper' && second == 'rock') || (first == 'paper' && second == 'spock') ||
   (first == 'scissors' && second == 'paper') || (first == 'scissors' && second == 'lizard') ||
   (first == 'lizard' && second == 'paper') || (first == 'lizard' && second == 'spock') ||
-  (first == 'spock' && second == 'scissors') || (first == 'spock' && second == 'rock')
+  (first == 'spock' && second == 'rock') || (first == 'spock' && second == 'scissors')
 end
 
-
-
-def display_results(player, computer)
-  if win?(player, computer)
-    prompt('You win this round!')  
-    $player_score = $player_score + 1
-    prompt("You have won #{$player_score} match(s)")
-  elsif win?(computer, player)
-    prompt('The computer wins this round!')
-    $computer_score = $computer_score + 1
-    prompt("Computer has won #{$computer_score} match(s)")
+def results(player, computer)
+  if winner?(player, computer)
+    prompt('Boom! You won this round!')
+  if winner?(computer, player)
+    prompt('Ouch! You lost this round.')
   else
-    prompt('Looks like a tie')
-    end
-end 
-
-def winner(p_s, c_s)
-  if p_s == 5
-    $player_score = 0
-    $computer_score = 0
-    prompt('YOU WON THE BEST OF 5 MATCHES!  YOU ARE THE GRAND CHAMPION OF THE UNIVERSE!!!')
-    prompt("YOU COULD PLAY AGAIN... OR, YOU CAN QUIT WHILE YOU'RE ON TOP- LIKE A BITCH!")
-  elsif c_s == 5
-    $player_score = 0
-    $computer_score = 0
-    prompt('LOSER! WTF?  YOU LET A COMPUTER BEAT YOU FIVE TIMES... SAD DAY FOR HUMANS.')
-    prompt('TRY AGAIN?... VENGEANCE IS A POWERFUL MOTIVATOR!')
+    prompt("It's a tie...\nNo points awarded.")
   end
 end
-    
-    
-Kernel.puts(); Kernel.puts("IT'S TIME TO PLAY ROCK-PAPER-SCISSORS-LIZARD-SPOCK"); Kernel.puts()   
-  
 
-loop do  
-  choice = ' '
-  loop do
-    prompt("Choose 'r' for rock, 'p' for paper, 'sc' for scissors, 'l' for lizard or 'sp' for spock...")
-    choice = Kernel.gets().chomp()
-    # choose one prompt could be replaced with the constnt
-    # example: prompt("choose one: #{VALID_CHOICES.join(', '}")
-    if choice == "r"
-      choice = "rock"
-      break
-    elsif choice == "p"
-      choice = "paper"
-      break
-    elsif choice == "sc"
-      choice = "scissors"
-      break
-    elsif choice == "l"
-      choice = "lizard"
-      break
-    elsif choice == "sp"
-      choice = "spock"
-      break
-    else
-      prompt("That's not a valid choice.")
-    end
+def score_keeper(result, score_keeper)
+  if result == 'Boom! You won this round!'
+    score_keeper[:player] += 1
+  elsif result == 'Ouch! You lost this round.'
+    score_keeper[:computer] += 1
   end
-  
-  computer_choice = VALID_CHOICES.sample
-  
-  puts '_________________________________________'
-  
-  prompt("You chose '#{choice}' and the COMPUTER chose '#{computer_choice}' ")
-    
-  display_results(choice, computer_choice)
-  puts '_________________________________________'
-
-  winner($player_score, $computer_score) 
-  
-  prompt("Do you want to play again? Y or N")
-  answer = Kernel.gets().chomp()
-  break unless answer.downcase().start_with?('y')
 end
 
+def score_output(score_keeper)
+  prompt("Curent score: #{score_keeper}")
+  if score_keeper[:player] == 5
+    prompt("WINNER! WINNER! CHICKEN DINNER! YOU BEAT THE EVIL COMPUTER!\nIf I were you, I would quit while I was ahead...")
+  elsif score_keeper[:computer] == 5
+    prompt("LOSER! WTF? You lost to a damn computer!\n Vengeance is one hell of a motivator...")
+  end
+end
 
+loop do
+  prompt("It's time to play Rock, PAPER, SCISSOR, LIZARD, SPOCK!!!!!!!!! \n  The best out of five rounds, WINS!! \n  You can type out the full name, or just the first two letters.")
+  
+  scores = { player:  0, computer:  0 }
+  until scores.value?(5)
+  
+    choice = ' '
+    loop do
+    
+      prompt("Choose your weapon: #{VALID_CHOICES.join(", ")}")
+      choice = gets.chomp
+    
+      if VALID_CHOICES.include?(choice)
+        break
+      elsif  ABBREVIATED_CHOICES.include?(choice)
+       choice = change_initial(choice)
+      else
+        prompt("Whatever you just did, didn't work.  Give it another shot, genius.")
+      end
+    end
+  
+  
+    computer_choice = VALID_CHOICES.sample
+  
+    prompt("You chose #{choice} and the computer chose #{computer_choice}w")
+  
+    winner = results(choice, computer_choice)
+  
+    prompt("#{winner}")
+  
+    score_keeper(winner, scores)
+  
+    score_output(scores)
+  end
+  
+  prompt('Play another round? Enter Y or N...')
+  again = gets.chomp
+  break unless again.downcase.start_with('y')
+  end
+end
